c15edf8c60b2df37f6c38e6ca2a6ae841343ec6d
Yingjie Shang <dev@yingjie.space>
2024-09-14 18:00:41 +0800

    refresh CPU to get current status

--- src/metrics/system_metrics.rs
+++ src/metrics/system_metrics.rs
@@ -1,4 +1,4 @@
-use std::sync::{Arc, Mutex};
+use std::sync::{atomic::AtomicU64, Arc, Mutex};
 
 use prometheus_client::metrics::gauge::Gauge;
 use prometheus_client::registry::Registry;
@@ -6,15 +6,15 @@ use sysinfo::System;
 use tokio::time::{self, Duration};
 
 pub struct SystemMetrics {
-    cpu_usage: Gauge<i64>,
-    memory_usage: Gauge<i64>,
+    cpu_usage: Gauge<f64, AtomicU64>,
+    memory_usage: Gauge<f64, AtomicU64>,
     system: Arc<Mutex<System>>,
 }
 
 impl SystemMetrics {
     pub fn new(registry: &mut Registry) -> Self {
-        let cpu_usage = Gauge::default();
-        let memory_usage = Gauge::default();
+        let cpu_usage = Gauge::<f64, AtomicU64>::default();
+        let memory_usage = Gauge::<f64, AtomicU64>::default();
 
         registry.register("cpu_usage_percent", "CPU usage percent", cpu_usage.clone());
         registry.register("memory_usage", "Memory usage percent", memory_usage.clone());
@@ -40,8 +40,9 @@ impl SystemMetrics {
     }
 
     fn collect_metrics(&self) {
-        let sys = self.system.lock().unwrap();
-        self.cpu_usage.set(sys.global_cpu_usage() as i64);
-        self.memory_usage.set(sys.used_memory() as i64);
+        let mut sys = self.system.lock().unwrap();
+        sys.refresh_cpu_all();
+        self.cpu_usage.set(sys.global_cpu_usage() as f64);
+        self.memory_usage.set(sys.used_memory() as f64);
     }
 }
